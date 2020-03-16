1b0fed25f8a0892db39541793587ad82157dd06e
Christine <christinebergersd@gmail.com>
2020-03-16 13:02:27 -0500

    Added transformation sql and tests for fema disasters

--- models/staging/fema/stg_fema__disasters_1994_2020.sql
+++ models/staging/fema/stg_fema__disasters_1994_2020.sql
@@ -2,4 +2,14 @@ with source as (
   select * from {{ source('fema', 'disasters_1994_2020') }}
 )
 
-select * from source
+transformed as (
+  select
+    incident_id,
+    substr(incident_id, -2) as incident_state,
+    date(extract(year from date), extract(month from date), 1) as incident_ym,
+    declaration_type as incident_declaration,
+    incident_type
+  from source
+)
+
+select * from transformed
