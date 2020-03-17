89f9237ea40315e5169a93fdf0032061978ae54a
Christine <christinebergersd@gmail.com>
2020-03-16 20:38:14 -0500

    Changed incident_state to incident_state code

--- models/staging/fema/stg_fema__disasters_1994_2020.sql
+++ models/staging/fema/stg_fema__disasters_1994_2020.sql
@@ -5,7 +5,7 @@ with source as (
 transformed as (
   select distinct
     incident_id,
-    substr(incident_id, -2) as incident_state,
+    substr(incident_id, -2) as incident_state_code,
     date(extract(year from date), extract(month from date), 1) as incident_ym,
     coalesce(declaration_type, 'NA') as incident_declaration,
     incident_type
